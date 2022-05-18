5a9d4b3958c04c529cc6a9dd1441588c1f331ed8
Stanlys <stanlys@yandex.ru>
2022-05-18 23:05:46 +0300

    feat: add mongoDB

--- express-server/server.js
+++ express-server/server.js
@@ -1,8 +1,13 @@
+import config from 'config';
 import express from 'express';
 import bodyParser from 'body-parser';
+import { default as mongodb } from 'mongodb';
+let MongoClient = mongodb.MongoClient;
+let ObjectID = mongodb.ObjectId;
 //const express = require('express');
 
 let app = express();
+let db;
 
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -37,16 +42,26 @@ app.get('/', (req, res) =>{
 });
 
 app.get('/projects', (req,res) =>{
-  res.send(projects);
+  //res.send(projects);
+  db.collection('projects').find().toArray( (err,all) =>{
+    if (err) {
+      console.error(err.message); 
+      return res.sendStatus(500);
+    }
+    res.send(all);
+  })
 });
 
 app.post('/projects', (req, res) =>{
   let new_project ={
-    id: Date.now(),
     name: req.body.name
   };
-  projects.push(new_project);
-  res.send(`You add new project ${new_project.name}`);
+  db.collection('projects').insert(new_project, (err, result) => {
+    if (err) {console.error(err.message); return res.sendStatus(500);}
+    res.send(new_project);
+  });
+  //projects.push(new_project);
+  
 });
 
 app.put('/projects/:id', (req,res)=>{
@@ -59,14 +74,26 @@ app.put('/projects/:id', (req,res)=>{
 })
 
 app.get('/projects/:id', (req,res) =>{
-  const id = Number(req.params.id);
-  let project = projects.find(el => el.id == id);
-  if (id>=projects.length) res.send('Sorry, but I am not found this project');
-  res.send(`Вы находитесь в проекте ${project.name} `);
+  const id = ObjectID(req.params.id);
+  db.collection('projects').findOne({_id: id}, (err, one) =>{
+    if (err) {
+      console.error(err.message); 
+      return res.sendStatus(500);
+    }
+
+    res.send(one);
+  });
+
 });
 
-app.listen(3030, ()=>{
-  console.log('Server start');
-})
+
+
+MongoClient.connect('mongodb://127.0.0.1:27017/projects', (err, database)=>{
+  if (err) return console.error(err.message);
+  db = database.db('projects');
+  app.listen(3030, ()=>{
+    console.log('Server start');
+  })
+});
 
 

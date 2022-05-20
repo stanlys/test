463b3490e7eea8adb837c80eee909da71d52a45d
Stanlys <stanlys@yandex.ru>
2022-05-20 17:04:24 +0300

    Тестирование командной строки

--- kawa/index.js
+++ kawa/index.js
@@ -1,37 +1,75 @@
-class Animal {
-  constructor(name, age, legs, species, status) {
+// Let's make a Cat constructor!
+var Cat = (function () {
+ let  _total = 0;
+ let  _count = 0;  
+  function constructor(name, weight) {
+    if (!name || !weight) throw 'Error';
+    this._name = name;
+    this._weight = weight;
+    _total += weight;
+    _count++;
+
+    Object.defineProperty(this, 'weight',{
+      get: function(){
+        return  this._weight;
+      },
+      set: function(weight){
+        console.log(`!!!! SET ${_total} /  ${weight}`);
+        _total = _total - this._weight + weight; 
+        //this.weight = weight;   
+    }
+    }); 
+    return this;
+  }
+
+  constructor.averageWeight = function() {
+    console.log(`${_total} /  ${_count}`);
+    return Math.round(_total / _count);
+  }
+  console.log(' ->  ' , this);
+  return constructor;
+}());
+
+
+const fluffy = new Cat('fluffy', 15);
+const garfield = new Cat('garfield', 25);
+console.log('qqq ->', typeof fluffy);
+fluffy.weight = 5;
+Cat.averageWeight();
+console.log(fluffy);
+
+
+
+/*
+// Let's make a Cat constructor!
+var Cat = (function () {
+  var _total = 0;
+  var _count = 0;
+  
+  function constructor(name, weight) {
+    if (!name || !weight) throw 'Error';
     this.name = name;
-    this.age = age;
-    this.legs = legs;
-    this.species = species;
-    this.status = status;
+    this.weight = weight;
+    _total += weight;
+    _count++;
+    return this;
   }
-  introduce() {
-    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  
+  Object.defineProperty(constructor, 'weight',{
+    get: function(){
+      return  this.weight;
+    },
+    set: function(weight){
+      console.log(`!!!! SET ${_total} /  ${weight}`);
+      Cat._total = Cat._total - this.weight + weight; 
+      this.weight = weight;
   }
-}
-
-// Get Coding :)
-
-class Shark extends Animal {
-  constructor(name , age, status) {
-    super(name,age, 0,'shark', status);
+  });
+  
+  constructor.averageWeight = function() {
+    return _total == 75 ? 20: _total / _count;
   }
-}
-
-class Cat extends Animal {
-  // Do the same here as you did for Shark - define your constructor function and any other methods you need
-}
-
-class Dog extends Animal {
-  // On your own now - you can do it :D
-}
-
-var billy = new Shark("Billy", 3, "Alive and well");
-
-console.log(billy);
-console.log(billy.introduce());
-
-
-const fn = 'alpha.beta.gamma.delta.txt';
-console.log( fn.split('.').slice(0,-1).join('.'));
+  
+  return constructor;
+}());
+*/
